018ca484bc84e9aa4407026241459db3edf2ef79
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-18 16:16:17 -0300

    adicionando paginaçao e ordenaçao

--- src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Cargo;
@@ -27,8 +31,10 @@ public class CrudFuncionarioService {
 	private final UnidadeTrabalhoRepository unidadeTrabalhoRepository;
 	
 	
-	public CrudFuncionarioService(FuncionarioRepository funcionarioRepository, 
-			CargoRepository cargoRepository, UnidadeTrabalhoRepository unidadeTrabalhoRepository) {
+	public CrudFuncionarioService(
+			FuncionarioRepository funcionarioRepository, 
+			CargoRepository cargoRepository, 
+			UnidadeTrabalhoRepository unidadeTrabalhoRepository) {
 		this.cargoRepository = cargoRepository;
 		this.funcionarioRepository = funcionarioRepository;
 		this.unidadeTrabalhoRepository = unidadeTrabalhoRepository;
@@ -53,7 +59,7 @@ public class CrudFuncionarioService {
 				atualizar(scanner);
 				break;
 			case 3:
-				visualizar();
+				visualizar(scanner);
 				break;
 			case 4:
 				deletar(scanner);
@@ -149,8 +155,16 @@ public class CrudFuncionarioService {
         System.out.println("Alterado");
 	}
 	
-	private void visualizar() {
-		Iterable<Funcionario> funcionarios = funcionarioRepository.findAll();
+	private void visualizar(Scanner scanner) {
+		System.out.println("Qual página você deseja vizualizar");
+		Integer page = scanner.nextInt();
+
+		Pageable pageable = PageRequest.of(page, 5, Sort.unsorted());
+		Page<Funcionario> funcionarios = funcionarioRepository.findAll(pageable);
+
+		System.out.println(funcionarios);
+		System.out.println("Página atual " + funcionarios.getNumber());
+		System.out.println("Total elemento " + funcionarios.getTotalElements());
 		funcionarios.forEach(funcionario -> System.out.println(funcionario));
 	}
 	
